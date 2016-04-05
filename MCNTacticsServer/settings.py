8184a75511b99ffe371caf3b6f2f523e0f39373c
jeremy2spade <jeremy2spade@gmail.com>
2016-04-06 02:19:06 +0900

    데이터베이스 변경 SQLite3 > MariaDB (MySQL)

--- MCNTacticsServer/settings.py
+++ MCNTacticsServer/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'MCNTacticsServer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mcn',
+        'USER': 'root',
+        'PASSWORD': 'mcn@2016',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
