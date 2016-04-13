8eef9b385d5f52284fbf0846f8defc3b41b8c5d8
jeremy2spade <jeremy2spade@gmail.com>
2016-04-14 03:43:16 +0900

    template, view 구현 유저 정보 가져오기

--- MCNTacticsServer/urls.py
+++ MCNTacticsServer/urls.py
@@ -1,21 +1,28 @@
 """MCNTacticsServer URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+The `urlpatterns` list routes URLs to viewss. For more information please see:
+	 https://docs.djangoproject.com/en/1.9/topics/http/urls/
 Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Function viewss
+	 1. Add an import:  from my_app import viewss
+	 2. Add a URL to urlpatterns:  url(r'^$', viewss.home, name='home')
+Class-based viewss
+	 1. Add an import:  from other_app.viewss import Home
+	 2. Add a URL to urlpatterns:  url(r'^$', Home.as_views(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+	 1. Import the include() function: from django.conf.urls import url, include
+	 2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, patterns
 from django.contrib import admin
+from polls import views
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+	
+	url(r'^common/$', views.index, name = 'index'),
+	url(r'^common/user_info/$', views.user_info, name = 'user_info'),
+	# url(r'^polls/(?P<user_id>\d+)/vote/$', views.vote, name = 'vote'),
+	# url(r'^polls/(?P<user_id>\d+)/result/$', views.result, name = 'result'),
+	url(r'^admin/', admin.site.urls),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from polls.models import User, Chessman
+
+def index(request):
+	user_list = User.objects.all().order_by('user_id')[:5]
+	context = {'user_list' : user_list}
+	return render(request, 'common/index.html', context)
+
+
+def user_info(request):
+	user_data = User.objects.all()
+	context = {'user_data' : user_data}
+	print '===CONTEXT==='
+	print context
+	return render(request, 'common/user_info.html', context)
+
 
 # Create your views here.
